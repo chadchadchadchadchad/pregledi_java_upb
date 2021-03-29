3e16d687fcdc8d27d03a985d3dbeeb581972d90a
Tomaz Cede <tomaz.cede@scv.si>
2021-03-29 11:31:55 +0200

    Doctors now shown in combobox (bug).

--- src/main/java/Check_up_panel.java
+++ src/main/java/Check_up_panel.java
@@ -15,16 +15,21 @@ public class Check_up_panel {
     private JComboBox doctor_combo;
     private static int id_p;
     private int id_worker;
+    private int id_zd;
+    private int id_doc;
     private DefaultListModel list = new DefaultListModel();
     private String[] workers;
     private String[] healthcenters;
+    private String[] doctors;
     private static JFrame frame;
 
     public void update()
     {
+        worker_combo.removeAllItems();
         list.clear();
 
         workers = database_check_up_panel.returnworkers(id_p);
+
         healthcenters = database_check_up_panel.returnhealhcenters();
 
         for (String name: workers) {
@@ -59,23 +64,31 @@ public class Check_up_panel {
         button_check.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+
                 button_check.setEnabled(false);
                 healthcenter_list.setEnabled(false);
-
+                doctor_combo.setEnabled(false);
+                doctor_combo.setSelectedItem(null);
                 worker_combo.setSelectedItem(null);
+                doctor_combo.removeAllItems();
             }
         });
-
-        /*list_check.addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                button_check.setEnabled(true);
-            }
-        });*/
         healthcenter_list.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                button_check.setEnabled(true);
+                //doctor_combo.removeAllItems();
+
+                id_zd = database_check_up_panel.returnzdid(healthcenter_list.getSelectedValue().toString());
+                doctors = database_check_up_panel.returndoctors(id_zd);
+
+                System.out.println(doctors[0]);
+
+                for (String d_name: doctors) {
+                    doctor_combo.addItem(d_name);
+                }
+
+                doctor_combo.setEnabled(true);
             }
         });
         add_em_button.addActionListener(new ActionListener() {
@@ -83,6 +96,18 @@ public class Check_up_panel {
             public void actionPerformed(ActionEvent e) {
                 Add_employee_form.main(id_p);
                 frame.dispose();
+            }
+        });
+        doctor_combo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = doctors[doctor_combo.getSelectedIndex()];
+
+                String[]name_surname = name.split(" ");
+
+                id_doc = database_check_up_panel.returndoctorid(name_surname[0], name_surname[1], id_zd);
+
+                button_check.setEnabled(true);
             }
         });
     }
--- src/main/java/database_check_up_panel.java
+++ src/main/java/database_check_up_panel.java
@@ -36,6 +36,57 @@ public class database_check_up_panel {
         return id;
     }
 
+    public static String[] returndoctors(int zd_id)
+    {
+        String[] names = new String[1];
+        try {
+            String uName = "rwnxlyblnkntlj";
+            String uPass= "465c86b2ff6199771cf8e82088e23ce686b7fb951d00fae60c6ac7dc87fe9091";
+            String host = "jdbc:postgresql://ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d89q761es01jua";
+
+            Class.forName("org.postgresql.Driver");
+            Connection c = DriverManager.getConnection(host, uName, uPass);
+
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM zdravniki WHERE zdravstveni_dom_id = " + zd_id + ";" );
+            int st = 0;
+            while ( rs.next() ) {
+                st++;
+            }
+            rs.close();
+            stmt.close();
+
+            Statement stmt2 = c.createStatement();
+            ResultSet rs2 = stmt2.executeQuery( "SELECT * FROM zdravniki WHERE zdravstveni_dom_id = " + zd_id + ";" );
+
+            names = new String[st];
+
+            int st2 = 0;
+            while ( rs2.next() ) {
+                String  name = rs2.getString("ime");
+                String surname = rs2.getString("priimek");
+
+                String fullname = name + " " + surname;
+
+                System.out.println( "Fullname = " + fullname );
+
+                names[st2] = fullname;
+
+                st2++;
+            }
+
+            rs2.close();
+            stmt2.close();
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+
+        return names;
+    }
+
     public static String[] returnworkers(int podjetje_id)
     {
         String[] names = new String[1];
@@ -89,6 +140,8 @@ public class database_check_up_panel {
         return names;
     }
 
+
+
     public static String[] returnhealhcenters()
     {
         String[] names = new String[1];
@@ -137,5 +190,69 @@ public class database_check_up_panel {
         }
 
         return names;
+    }
+
+    public static Integer returnzdid(String imezd)
+    {
+        int id = 0;
+        try {
+            String uName = "rwnxlyblnkntlj";
+            String uPass= "465c86b2ff6199771cf8e82088e23ce686b7fb951d00fae60c6ac7dc87fe9091";
+            String host = "jdbc:postgresql://ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d89q761es01jua";
+
+            Class.forName("org.postgresql.Driver");
+            Connection c = DriverManager.getConnection(host, uName, uPass);
+
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT id FROM zdravstveni_domovi WHERE ime = '" + imezd + "';" );
+            while ( rs.next() ) {
+                int  idp = rs.getInt("id");
+
+                System.out.println( "ID = " + idp );
+
+                id = idp;
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+
+        return id;
+    }
+
+    public static Integer returndoctorid(String imed, String priimekd, int id_zd)
+    {
+        int id = 0;
+        try {
+            String uName = "rwnxlyblnkntlj";
+            String uPass= "465c86b2ff6199771cf8e82088e23ce686b7fb951d00fae60c6ac7dc87fe9091";
+            String host = "jdbc:postgresql://ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d89q761es01jua";
+
+            Class.forName("org.postgresql.Driver");
+            Connection c = DriverManager.getConnection(host, uName, uPass);
+
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT id FROM zdravniki WHERE ime = '" + imed + "' AND priimek = '" + priimekd + "' AND zdravstveni_dom_id = "+ id_zd +";" );
+            while ( rs.next() ) {
+                int  idp = rs.getInt("id");
+
+                System.out.println( "ID = " + idp );
+
+                id = idp;
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+
+        return id;
     }
 }
